23a5005e2d24f900b73d4729c01dfeb8f3ff2c32
shub420s <144937257+shub420s@users.noreply.github.com>
2024-07-18 07:32:20 +0530

    at home

---
+++ DOM_byme/script1.js
@@ -0,0 +1,9 @@
+const tierInput = document.getElementById('tier');
+
+const imageForm = document.getElementById('image-form');
+
+const submitBtn = document.getElementById('submit');
+
+imageForm.addEventListener('submit',(event) => {
+        event.preventDefault();
+})
